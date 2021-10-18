33b7ee96b45b18bcfec8495fe65d778a40460466
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2021-10-18 23:54:05 +0300

    code refactoring

--- lib/screens/second_page.dart
+++ lib/screens/second_page.dart
@@ -4,37 +4,50 @@ import 'package:provider/provider.dart';
 
 import '../language_change.dart';
 
-
 class SecondPage extends StatelessWidget {
   const SecondPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     LanguageChange controller = context.read<LanguageChange>();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("second_page_appbar_title").tr(),
-
-      ),body:Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("second_page_info").tr(),
-          SizedBox(height: 20,),
-          Text("choose_language_text").tr(),
-          TextButton(onPressed: (){
-            // ignore: deprecated_member_use
-            context.locale = Locale('en', 'US');
-            controller.onLanguageChanged();
-          }, child: Text("English")),
-          TextButton(onPressed: (){
-            // ignore: deprecated_member_use
-            context.locale = Locale('tr', 'TR');
-            controller.onLanguageChanged();
-          }, child: Text("Türkçe"))
-        ],
+      appBar: secondPageAppbar(),
+      body: Center(
+        child: buildColumn(context, controller),
       ),
-    ),
+    );
+  }
+
+  AppBar secondPageAppbar() {
+    return AppBar(
+      title: Text("second_page_appbar_title").tr(),
+    );
+  }
+
+  Widget buildColumn(BuildContext context, LanguageChange controller) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("second_page_info").tr(),
+        SizedBox(
+          height: 20,
+        ),
+        Text("choose_language_text").tr(),
+        TextButton(
+            onPressed: () {
+              context.setLocale(Locale('en', 'US'));
+              controller.onLanguageChanged();
+            },
+            child: Text("English")),
+        TextButton(
+            onPressed: () {
+              context.setLocale(Locale('tr', 'TR'));
+
+              controller.onLanguageChanged();
+            },
+            child: Text("Türkçe"))
+      ],
     );
   }
 }
