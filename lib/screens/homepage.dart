740aafd2f179713b04a4ef7730d4c9816f70d3dc
Mehmet Emre ÖZ <nyz_memre_09@hotmail.com>
2021-10-18 23:23:57 +0300

    multi language run with provider

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-
+import 'package:provider/provider.dart';
+import '../language_change.dart';
 import 'second_page.dart';
 
 
@@ -10,18 +11,19 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    context.watch<LanguageChange>();
     return Scaffold(
       appBar: AppBar(
-        title: Text('homepage_appbar_title'.tr())
+        title: Text('homepage_appbar_title').tr()
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("homepage_body".tr()),
+            Text("homepage_body").tr(),
 TextButton(onPressed: (){
   Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>SecondPage()));
-}, child: Text('second_page_appbar_title'.tr()))          ],
+}, child: Text('second_page_appbar_title').tr())          ],
         )
       )
     );
--- lib/screens/second_page.dart
+++ lib/screens/second_page.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:provider/provider.dart';
+
+import '../language_change.dart';
 
 
 class SecondPage extends StatelessWidget {
   const SecondPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    LanguageChange controller = context.read<LanguageChange>();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("second_page_appbar_title".tr()),
+        title: Text("second_page_appbar_title").tr(),
 
       ),body:Center(
-      child: Text("second_page_info".tr()),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("second_page_info").tr(),
+          SizedBox(height: 20,),
+          Text("choose_language_text").tr(),
+          TextButton(onPressed: (){
+            // ignore: deprecated_member_use
+            context.locale = Locale('en', 'US');
+            controller.onLanguageChanged();
+          }, child: Text("English")),
+          TextButton(onPressed: (){
+            // ignore: deprecated_member_use
+            context.locale = Locale('tr', 'TR');
+            controller.onLanguageChanged();
+          }, child: Text("Türkçe"))
+        ],
+      ),
     ),
     );
   }
